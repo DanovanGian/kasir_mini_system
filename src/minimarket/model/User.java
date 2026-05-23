2b12b1a6760cf6dec3c2fce26685f913f6cf296d
DanovanGian <dodogian005@gmail.com>
2026-05-23 15:23:46 +0700

    user class

--- src/minimarket/model/User.java
+++ src/minimarket/model/User.java
@@ -5,6 +5,25 @@ package minimarket.model;
 public class User {
     private int id;
     private String username;
-    private String password
+    private String password;
+    private String role;
+    private String nama;
+
+    public User(int id, String username, String password, String role, String nama){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role; //admin atau kasir atau role lain jika in case ada role baru
+        this.nama = nama;
+
+    }
+
+    //getter dan setter untuk semua atribut yang ada di class user
+    public int getId() { return id; }
+    public String getUsername() { return username; }
+    public String getPassword() {return password;}
+    public String getRole() { return role; }
+    public String getNama() { return nama; }
     
+
 }
