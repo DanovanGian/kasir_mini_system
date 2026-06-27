b7451715e5f1f99cbb95dedb39408206cea33fc6
DanovanGian <dodogian005@gmail.com>
2026-06-27 23:32:10 +0700

    getter setter

--- src/minimarket/model/DetailTransaksi.java
+++ src/minimarket/model/DetailTransaksi.java
@@ -12,8 +12,24 @@ public class DetailTransaksi {
         this.jumlah = jumlah;
         this.hargaSatuan = hargaSatuan;
 
+    }
+    //getter dan setter 
+    public int getidTransaksi () { return idTransaksi; }
+    public int getidBarang () { return idBarang; }
+    public int getjumlah () { return jumlah; }
+    public double gethargaSatuan () { return hargaSatuan; }
 
-        
+    public void setidTransaksi ( int idTransaksi ){
+        this.idTransaksi = idTransaksi;
+    }
+    public void setidBarang ( int Barang ){
+        this.idBarang = idBarang;
+    }
+    public void setjumlah ( int Jumlah ){
+        this.jumlah  = jumlah;
+    }
+    public void sethargaSatuan ( int hargaSatuan ){
+        this.hargaSatuan = hargaSatuan;
     }
     
     
