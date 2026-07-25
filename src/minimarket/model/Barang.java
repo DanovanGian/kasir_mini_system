09b21be9df58d0b43230c8727ad84d909c234bf4
DanovanGian <dodogian005@gmail.com>
2026-07-25 17:46:26 +0700

    progress barang

--- src/minimarket/model/Barang.java
+++ src/minimarket/model/Barang.java
@@ -1,23 +1,34 @@
 package minimarket.model;
-
+ 
 public class Barang {
     private int id;
     private String kodeBarang;
-    private String  namaBarang;
-    private double hargaBarang;
+    private String namaBarang;
+    private double harga;
     private int stok;
     private String kategori;
-
-    public Barang(int id, String kodeBarang, String namaBarang, int hargaBarang, int stok, String kategori)
-    {
+ 
+    public Barang(int id, String kodeBarang, String namaBarang, double harga, int stok, String kategori) {
         this.id = id;
         this.kodeBarang = kodeBarang;
         this.namaBarang = namaBarang;
-        this.hargaBarang = hargaBarang;
+        this.harga = harga;
         this.stok = stok;
         this.kategori = kategori;
-    
-
     }
-    
-}
+ 
+    // Getter & Setter
+    public int getId()            { return id; }
+    public String getKodeBarang() { return kodeBarang; }
+    public String getNamaBarang() { return namaBarang; }
+    public double getHarga()      { return harga; }
+    public int getStok()          { return stok; }
+    public String getKategori()   { return kategori; }
+ 
+    public void setHarga(double harga)         { this.harga = harga; }
+    public void setStok(int stok)              { this.stok = stok; }
+    public void setNamaBarang(String nama)     { this.namaBarang = nama; }
+ 
+    public boolean isStokTersedia(int jumlah) {
+        return stok >= jumlah;
+    }
