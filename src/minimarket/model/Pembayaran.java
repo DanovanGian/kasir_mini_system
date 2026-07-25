add186d2199b386eae2e8eb65d7359cc345374bf
DanovanGian <dodogian005@gmail.com>
2026-07-26 00:23:22 +0700

    konstruktor nya

--- src/minimarket/model/Pembayaran.java
+++ src/minimarket/model/Pembayaran.java
@@ -4,4 +4,10 @@ public class Pembayaran {
     private double totalBayar;
     private double uangBayar;
     private double kembalian;
+
+    public Pembayaran(double totalBayar, double uangBayar) {
+        this.totalBayar = totalBayar;
+        this.uangBayar  = uangBayar;
+        this.kembalian  = uangBayar - totalBayar;
+    }
 }
