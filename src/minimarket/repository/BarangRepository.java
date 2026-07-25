ea97f9f8e8900b2f70fc4bbe3be32b467e603e0b
DanovanGian <dodogian005@gmail.com>
2026-07-26 00:29:04 +0700

    repo barang

--- src/minimarket/repository/BarangRepository.java
+++ src/minimarket/repository/BarangRepository.java
@@ -1,5 +1,20 @@
 package minimarket.repository;
-
+ 
+import minimarket.model.Barang;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+ 
 public class BarangRepository {
-    
+    private List<Barang> barangList = new ArrayList<>();
+    private int nextId = 1;
+ 
+    public BarangRepository() {
+        // Data dummy
+        tambah(new Barang(nextId++, "MKN001", "Indomie Goreng",     3500,  100, "Makanan"));
+        tambah(new Barang(nextId++, "MNM001", "Teh Botol 350ml",    5000,  120, "Minuman"));
+        tambah(new Barang(nextId++, "MNM002", "Aqua 600ml",         4000,  200, "Minuman"));
+        tambah(new Barang(nextId++, "SNK001", "Chitato 75g",        10000,  50, "Snack"));
+        tambah(new Barang(nextId++, "KBR001", "Sabun Lifebuoy",     8500,   70, "Kebersihan"));
+    }
 }
--- src/minimarket/repository/UserRepository.java
+++ src/minimarket/repository/UserRepository.java
@@ -1,28 +1,30 @@
 package minimarket.repository;
- 
+
 import minimarket.model.User;
 import java.util.ArrayList;
 import java.util.List;
- 
+
 public class UserRepository {
     private List<User> users = new ArrayList<>();
- 
+
     public UserRepository() {
         // Data dummy
-        users.add(new User(1, "admin",  "admin123",  "Administrator", "admin"));
-        users.add(new User(2, "kasir1", "kasir123",  "Budi Santoso",  "kasir"));
-        users.add(new User(3, "kasir2", "kasir123",  "Siti Rahayu",   "kasir"));
+        users.add(new User(1, "admin", "admin123", "Administrator", "admin"));
+        users.add(new User(2, "kasir1", "kasir123", "Budi Santoso", "kasir"));
+        users.add(new User(3, "kasir2", "kasir123", "Siti Rahayu", "kasir"));
     }
 
     public User findByUsername(String username) {
         return users.stream()
-            .filter(u -> u.getUsername().equals(username))
-            .findFirst()
-            .orElse(null);
+                .filter(u -> u.getUsername().equals(username))
+                .findFirst()
+                .orElse(null);
     }
+
     public User login(String username, String password) {
         User user = findByUsername(username);
-        if (user != null && user.getPassword().equals(password)) return user;
+        if (user != null && user.getPassword().equals(password))
+            return user;
         return null;
     }
 
