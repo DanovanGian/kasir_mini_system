3aecf5cbfee925bb8a80d7689014cc2b588697c9
DanovanGian <dodogian005@gmail.com>
2026-07-26 21:43:13 +0700

    formatnya

--- src/minimarket/util/StrukPrinter.java
+++ src/minimarket/util/StrukPrinter.java
@@ -3,3 +3,15 @@ package minimarket.util;
 import minimarket.model.DetailTransaksi;
 import minimarket.model.Pembayaran;
 import minimarket.model.Transaksi;
+public class StrukPrinter {
+ 
+    public static void cetak(Transaksi trx, Pembayaran bayar) {
+        int LEBAR = 40;
+        System.out.println(FormatHelper.garis(LEBAR));
+        System.out.printf("%s%n", center("MINIMARKET DEL", LEBAR));
+        System.out.printf("%s%n", center("Jl. Sudirman No. 1", LEBAR));
+        System.out.println(FormatHelper.garis(LEBAR));
+        System.out.printf("Kasir   : %s%n", trx.getKasir().getNama());
+        System.out.printf("Tanggal : %s%n", trx.getTanggalFormatted());
+        System.out.printf("No. Trx : TRX-%04d%n", trx.getIdTransaksi());
+        System.out.println(FormatHelper.garisTengah(LEBAR));
