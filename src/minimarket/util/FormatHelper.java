10b430244451038b2412e8728cf68c71c11a52da
DanovanGian <dodogian005@gmail.com>
2026-07-26 21:39:52 +0700

    format helper

--- src/minimarket/util/FormatHelper.java
+++ src/minimarket/util/FormatHelper.java
@@ -1,5 +1,20 @@
 package minimarket.util;
-
+ 
+import java.text.NumberFormat;
+import java.util.Locale;
+ 
 public class FormatHelper {
-    
+    private static final NumberFormat IDR = NumberFormat.getInstance(new Locale("id", "ID"));
+ 
+    public static String rupiah(double amount) {
+        return "Rp" + IDR.format((long) amount);
+    }
+ 
+    public static String garis(int panjang) {
+        return "=".repeat(panjang);
+    }
+ 
+    public static String garisTengah(int panjang) {
+        return "-".repeat(panjang);
+    }
 }
